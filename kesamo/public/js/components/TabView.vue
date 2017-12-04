72db5187cef877ad6d3d8c4e391657ca31dca2da
jumtech <despairing.clock+jumGithub@gmail.com>
2017-12-04 12:03:32 +0900

    tab color changes when selected

--- kesamo/public/js/components/TabView.vue
+++ kesamo/public/js/components/TabView.vue
@@ -1,17 +1,46 @@
-<template lang="pug">
+<template lang='pug'>
 .container
-  router-link(to='routine')
-    | □
-  router-link(to='routines')
-    | ≡
+  .tab-item(v-bind:class='{ selected: isRoutineSelected }' @click='select("routine")')
+    .tab-link
+      router-link(to='routine')
+        | □
+  .tab-item(v-bind:class='{ selected: !isRoutineSelected }' @click='select("routines")')
+    .tab-link
+      router-link(to='routines')
+        | ≡
 </template>
 
-<style lang="stylus" scoped>
-a {
-  font-size: 100px;
-}
+<style lang='stylus' scoped>
+.container
+  display flex
+  width 100%
+  height 100px
+  padding 5px 0 5px 0
+.tab-item
+  flex-grow 1
+  text-align center
+  display flex
+  justify-content center
+  align-items center
+  &.selected .tab-link a
+    color red
+.tab-link a
+  font-size 5.4rem
 </style>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      isRoutineSelected: true
+    };
+  },
+  methods: {
+    select(tabName) {
+      this.isRoutineSelected = tabName === 'routine' ? true : false;
+      console.log("this.selisRoutineSelectedected: ",this.isRoutineSelected);
+      return;
+    },
+  },
+};
 </script>
