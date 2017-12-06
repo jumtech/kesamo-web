7a7a4d9c89de86c1b165436591fb139a246d9212
jumtech <despairing.clock+jumGithub@gmail.com>
2017-12-06 14:34:17 +0900

    [routineDetail]add right/left arrow

--- kesamo/public/js/components/RoutineDetailView.vue
+++ kesamo/public/js/components/RoutineDetailView.vue
@@ -3,8 +3,10 @@
   template(v-if='routineValues && routineValues.length > 0')
     p.title
       | {{routineValues[currentIndex].title}}
-    .left.side(v-if='currentIndex > 0' @click='go(-1)')
-    .right.side(v-if='currentIndex < routineValues.length - 1' @click='go(1)')
+    .side-tap-area.left(v-if='currentIndex > 0' @click='go(-1)')
+      .arrow-icon.left
+    .side-tap-area.right(v-if='currentIndex < routineValues.length - 1' @click='go(1)')
+      .arrow-icon.right
 </template>
 
 <style lang='stylus' scoped>
@@ -12,14 +14,28 @@
   display flex
   justify-content center
   align-items center
-  & .side
-    width 80px
+  & .side-tap-area
+    width 120px
     height calc(100vh - 60px)
     position absolute
+    display flex
+    justify-content center
+    align-items center
     &.left
       left 0
     &.right
       right 0
+    & .arrow-icon
+      width 15px
+      height 15px
+      box-sizing border-box
+      border 15px solid transparent
+      &.left
+        margin-right 90px
+        border-right 15px solid #B80228
+      &.right
+        margin-left 90px
+        border-left 15px solid #B80228
   & .title
     font-size 2.4rem
 </style>
